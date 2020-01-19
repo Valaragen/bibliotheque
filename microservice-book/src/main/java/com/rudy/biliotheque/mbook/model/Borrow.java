e6a1837de6b7c471b9a3331900425f251c73b45c
Valaragen <rudy.boinnard@gmail.com>
2020-01-19 18:47:02 +0100

    persistence annotations on model

--- microservice-book/src/main/java/com/rudy/biliotheque/mbook/model/Borrow.java
+++ microservice-book/src/main/java/com/rudy/biliotheque/mbook/model/Borrow.java
@@ -3,11 +3,33 @@ package com.rudy.biliotheque.mbook.model;
 import com.rudy.biliotheque.mbook.model.common.AbstractEntity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @Entity
 public class Borrow extends AbstractEntity {
+    @ManyToOne
+    private Book book;
+
+    @ManyToOne
+    private User user;
+
+    private Boolean hasDurationExtended;
+
+    @UpdateTimestamp
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+    @Temporal(TemporalType.TIMESTAMP)
+    private java.util.Date loanStartDate;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+    @Temporal(TemporalType.TIMESTAMP)
+    private java.util.Date loanEndDate;
+
 }
